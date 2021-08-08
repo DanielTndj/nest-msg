db7d35531cb292ac98dc6fc1c867e19e1961f996
Daniel Tanudjaja <daniel.tanudjaja@computradetech.com>
2021-08-08 16:04:48 +0700

    nest project

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
+  @Get()
+  getListMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) throw new NotFoundException('message not found');
+
+    return message;
+  }
+
+  @Post()
+  createMessage(@Body() { content }: CreateMessageDto) {
+    return this.messagesService.create(content);
+  }
+}
